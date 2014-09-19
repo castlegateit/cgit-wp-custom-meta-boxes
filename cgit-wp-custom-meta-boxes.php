c5ac120eee97c87a2a294f0696c0332b38a0d35f
John Hughes <john@castlegateit.co.uk>
2014-09-19 11:04:16 +0100

    Add plugin description

--- cgit-wp-custom-meta-boxes.php
+++ cgit-wp-custom-meta-boxes.php
@@ -4,7 +4,7 @@
 
 Plugin Name: Castlegate IT WP Custom Meta Boxes
 Plugin URI: http://github.com/castlegateit/cgit-wp-custom-meta-boxes
-Description:
+Description: Plugin wrapper for Human Made Custom Meta Boxes.
 Version: 1.0
 Author: Castlegate IT
 Author URI: http://www.castlegateit.co.uk/
