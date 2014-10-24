8272c0452560a9731937060b67452f7eba113f26
John Hughes <john@castlegateit.co.uk>
2014-10-24 11:07:36 +0100

    Add convenient functions to get field values

--- cgit-wp-custom-meta-boxes.php
+++ cgit-wp-custom-meta-boxes.php
@@ -13,3 +13,35 @@ License: MIT
 */
 
 require_once 'custom-meta-boxes/custom-meta-boxes.php';
+
+/**
+ * Get field value
+ *
+ * Provides a convenient way of accessing CMB field values, based on the
+ * Advanced Custom Fields. Uses the current post ID by default (if available);
+ * returns FALSE if post ID is not set.
+ */
+function get_field ($field, $post_id = FALSE) {
+
+    if ( $post_id == FALSE ) {
+
+        global $post;
+
+        if ( isset($post) ) {
+            $post_id = $post->ID;
+        } else {
+            return FALSE;
+        }
+
+    }
+
+    return get_post_meta($post_id, $field, TRUE);
+
+}
+
+/**
+ * Print field value
+ */
+function the_field ($field, $post_id = FALSE) {
+    echo get_field($field, $post_id);
+}
