2fcfe2cd2a1e46a666d5f9659731f8dfc2f1f1cc
John Hughes <john@castlegateit.co.uk>
2014-12-10 14:43:39 +0000

    Remove submodule and make plugin provide convenience functions instead of acting as a wrapper

--- cgit-wp-custom-meta-boxes.php
+++ cgit-wp-custom-meta-boxes.php
@@ -13,37 +13,6 @@ License: MIT
 */
 
 /**
- * Activation hook
- *
- * Check that the custom meta boxes plugin is installed before activation.
- */
-function activate()
-{
-    if (!is_plugin_active('custom-meta-boxes/custom-meta-boxes.php')) {
-
-        $message = 'This plugin requires the <code>custom meta boxes</code> ';
-        $message.= ' plugin. Please ';
-        $message.= 'ensure you\'ve installed it in the correct location: <code>';
-        $message.= 'plugins/custom-meta-boxes</code><br /><br />When ';
-        $message.= 'download via GitHub\'s web interface, the installation ';
-        $message.= 'directory may be <code>plugins/Custom-Meta-Boxes-master';
-        $message.=  '</code>. Be sure to remove <code>master</code> from the ';
-        $message.= 'directory and convert to lower case.<br /><br />Download ';
-        $message.= 'from <a target="_blank" href="https://github.com/humanmade';
-        $message.= '/Custom-Meta-Boxes">GitHub</a>';
-        wp_die($message);
-    }
-}
-register_activation_hook(__FILE__, 'activate');
-
-/**
- * Include the custom meta boxes plugin
- */
-if (file_exists('custom-meta-boxes/custom-meta-boxes.php')) {
-    require_once('custom-meta-boxes/custom-meta-boxes.php');
-}
-
-/**
  * Get field value
  *
  * Provides a convenient way of accessing CMB field values, based on the
